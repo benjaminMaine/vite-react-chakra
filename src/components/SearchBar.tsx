d22945228e12d3eb85d143c11c4f50dc299fb97e
benjaminMaine <benjamin.maine@gmail.com>
2023-12-01 10:04:17 +0100

    [4] Bad #1 refetch

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,24 @@
+import { ChangeEventHandler } from 'react';
+
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+
+export const SearchBar = ({
+  searchText,
+  onChangeSearchText,
+}: {
+  searchText: string;
+  onChangeSearchText: ChangeEventHandler<HTMLInputElement>;
+}) => (
+  <InputGroup size="md">
+    <Input
+      placeholder="Search.."
+      value={searchText}
+      onChange={onChangeSearchText}
+    />
+    {/* <InputRightElement width="4.5rem" pr={3}> */}
+    {/*   <Button h="1.75rem" size="sm"> */}
+    {/*     Search */}
+    {/*   </Button> */}
+    {/* </InputRightElement> */}
+  </InputGroup>
+);
