188894cb1b1731c1fea4476be848f0343188bfcd
benjaminMaine <benjamin.maine@gmail.com>
2023-12-01 09:59:13 +0100

    [1] with optimistic update

--- src/requests/posts/useCreatePost.tsx
+++ src/requests/posts/useCreatePost.tsx
@@ -1,24 +1,51 @@
 import axios from 'axios';
 
-import { MutateOptions, useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { Post } from '../../types/post';
+import { QUERY_KEYS } from '../keys';
 
 type CreatePostDTO = Omit<Post, 'id'>;
 
-const mutationFn = (data: CreatePostDTO): Promise<Post> =>
+const mutationFn = (newPostDTO: CreatePostDTO): Promise<Post> =>
   axios
-    .post('http://localhost:5001/posts', data, {
+    .post('http://localhost:5001/posts', newPostDTO, {
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
       },
     })
     .then((res) => res.data);
 
-export const useCreatePost = (
-  options: MutateOptions<Post, unknown, CreatePostDTO> | undefined = undefined
-) =>
-  useMutation<Post, unknown, CreatePostDTO>({
+export const useCreatePost = (options = {}) => {
+  const queryClient = useQueryClient();
+  return useMutation<Post, unknown, CreatePostDTO, Post[]>({
     mutationFn,
+    onMutate: async (newPostDTO) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({
+        queryKey: [QUERY_KEYS.POSTS.FIND_ALL],
+      });
+
+      // Snapshot the previous value
+      const previousPosts = queryClient.getQueryData<Post[]>([
+        QUERY_KEYS.POSTS.FIND_ALL,
+      ]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData<Post[]>(
+        [QUERY_KEYS.POSTS.FIND_ALL],
+        (oldPosts = []) => [...oldPosts, { ...newPostDTO, id: 0 }]
+      );
+
+      return previousPosts;
+    },
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(['todos'], context);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.POSTS.FIND_ALL] });
+    },
     ...options,
   });
+};
